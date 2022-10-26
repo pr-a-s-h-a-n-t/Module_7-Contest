ec2d992179a25e550495ca389d4873b79495be97
Prashant Mishra <prashantmishra111@outlook.com>
2022-10-26 22:28:46 +0530

    ui completed

---
+++ contest-solution-react/my-product-card/src/components/Component.js
@@ -0,0 +1,19 @@
+import ProductCart from "./ProductCart";
+
+
+
+function Component() {
+    return  (
+        // name of jsx start with small letter.
+        //  name of react component with capital letter
+         <div>
+             
+             <ProductCart />
+              
+
+         </div>
+         
+    );
+}
+
+export default Component;
---
+++ contest-solution-react/my-product-card/src/index.js
@@ -0,0 +1,14 @@
+// Jai shree Ganesh!!!!!!
+
+ 
+import ReactDom from 'react-dom';
+import './index.css';
+import Component from './components/Component'
+// import Component from './components/Component';
+
+
+
+
+
+
+ReactDom.render(<Component />, document.getElementById('root'));
